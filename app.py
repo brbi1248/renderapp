24cf7bc0817eab4ecccf223a706b372f93c749c5
Brittany Bilotti <brbi1248@colorado.edu>
2023-03-21 17:39:23 +0000

    db_test_corrected

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello_world():
     return 'Hello, World!'
 
 @app.route('/db_test')
-def testing:
+def testing():
     conn = psycopg2.connect("postgres://brbi1248_db_user:cnQbJbn0p8hK9SFfSVJ10oBYUhFkMVEv@dpg-cgcug2m4dad6fr7dvs20-a/brbi1248_db")
     conn.close()
     return "Database Connection was successful"
