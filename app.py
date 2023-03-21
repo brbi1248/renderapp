64f66817f077ea825bb885645af9e2fc622d3074
Brittany Bilotti <brbi1248@colorado.edu>
2023-03-21 17:46:33 +0000

    db_create

--- app.py
+++ app.py
@@ -12,4 +12,20 @@ def hello_world():
 def testing():
     conn = psycopg2.connect("postgres://brbi1248_db_user:cnQbJbn0p8hK9SFfSVJ10oBYUhFkMVEv@dpg-cgcug2m4dad6fr7dvs20-a/brbi1248_db")
     conn.close()
-    return "Database Connection was successful"
+    return "Database Connection Successful"
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgres://brbi1248_db_user:cnQbJbn0p8hK9SFfSVJ10oBYUhFkMVEv@dpg-cgcug2m4dad6fr7dvs20-a/brbi1248_db")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
